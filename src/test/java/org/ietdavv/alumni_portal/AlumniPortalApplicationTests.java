13b98a3d0e9ceede4a94f7b638cf2129e5307bbc
Tanmay Shukla <tanmay.shukla629@gmail.com>
2024-10-05 18:11:30 +0530

    Laying down the foundation

---
+++ src/test/java/org/ietdavv/alumni_portal/AlumniPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ietdavv.alumni_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumniPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
